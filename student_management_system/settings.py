5c622a7ab79bf68ffb028098d1ff9922fefb28d5
JInjaBread <82684344+JInjaBread@users.noreply.github.com>
2022-02-02 10:20:48 +0800

    Update settings.py

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '(i#*06f#keydy_fh17bf=$0f6v)^wr^l7*u4gq42m*sztu#2_m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pigtail.herokuapp.com/']
+ALLOWED_HOSTS = ['pigtail.herokuapp.com']
 
 NUMB_TURN_CREDENTIAL = config('NUMB_TURN_CREDENTIAL', default=None)
 NUMB_TURN_USERNAME = config('NUMB_TURN_USERNAME', default=None)
